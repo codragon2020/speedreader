2fb8b28f45720784af74b5163646461af46d54bf
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-03 06:56:26 -0500

    Added quote and split the words

---
+++ script.js
@@ -0,0 +1,3 @@
+var quote = "Our deepest fear is not that we are inadequate. Our deepest fear is that we are powerful beyond measure. It is our Light, not our Darkness, that most frightens us."
+
+var words = poem.split(" ");
