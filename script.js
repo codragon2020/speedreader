0e486e38f1eccb63fa2b0e559c64fdb8d8b5c7ad
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-03 06:58:58 -0500

    Added variables to connect the elements in html to my code

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 var quote = "Our deepest fear is not that we are inadequate. Our deepest fear is that we are powerful beyond measure. It is our Light, not our Darkness, that most frightens us."
-
 var words = poem.split(" ");
+
+// Variables to utilize the elements by Id in html
+var mainEl = document.getElementById("main");
+var timerEl = document.getElementById("countdown");
+var bodyEl = document.createElement("div");
